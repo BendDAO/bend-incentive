fe03350de2ea86f4df14ffd717afd21df6c919ef
onigirisan <onigirisan.chen@gmail.com>
2021-10-17 20:51:48 +0800

    finish bend token test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,10 @@
 import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-import { task } from "hardhat/config";
 import dotenv from "dotenv";
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+import { accounts } from "./test-wallets";
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -30,7 +21,9 @@ export default {
   },
   networks: {
     hardhat: {
+      chainId: 31337,
       initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+      accounts: accounts,
     },
     rinkeby: {
       url: process.env.RINKEBY_URL || "",
