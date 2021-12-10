468e8b4b0b082827f37145846157c8513725cb96
onigirisan <onigirisan.chen@gmail.com>
2021-12-10 13:28:59 +0800

    add config asset for deploy scripts

--- contracts/stake/interfaces/IStakedToken.sol
+++ contracts/stake/interfaces/IStakedToken.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
-import {
-    IERC20Upgradeable
-} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import {DistributionTypes} from "../DistributionTypes.sol";
 
 interface IStakedToken is IERC20Upgradeable {
     function configureAssets(
         DistributionTypes.AssetConfigInput[] calldata assetsConfigInput
     ) external;
+
+    function configure(uint128 emissionPerSecond) external;
 
     function stake(address to, uint256 amount) external;
 
