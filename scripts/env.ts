038951b0f3c3dfdb6386cd7aa0df58129f0fb727
Developer Bend <developerbend@gmail.com>
2021-12-28 11:16:09 +0800

    delete useless code

--- scripts/env.ts
+++
@@ -1,6 +0,0 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-export let HRE: HardhatRuntimeEnvironment = {} as HardhatRuntimeEnvironment;
-
-export const setHRE = (_HRE: HardhatRuntimeEnvironment) => {
-  HRE = _HRE;
-};
