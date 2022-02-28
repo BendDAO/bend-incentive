021dce897144df59382180224e2833686a0db464
akashiceth <akashiceth@gmail.com>
2022-02-28 23:52:26 +0800

    add lockup bend

--- contracts/incentives/interfaces/IWETH.sol
+++ contracts/incentives/interfaces/IWETH.sol
@@ -2,15 +2,19 @@
 pragma solidity ^0.8.0;
 
 interface IWETH {
-  function deposit() external payable;
+    function deposit() external payable;
 
-  function withdraw(uint256) external;
+    function withdraw(uint256) external;
 
-  function approve(address guy, uint256 wad) external returns (bool);
+    function approve(address, uint256) external returns (bool);
 
-  function transferFrom(
-    address src,
-    address dst,
-    uint256 wad
-  ) external returns (bool);
+    function transfer(address, uint256) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
+
+    function balanceOf(address) external view returns (uint256);
 }
