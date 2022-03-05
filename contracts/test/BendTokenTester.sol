768ddd3238788d0fae9d3424b45bf5957b749429
akashiceth <akashiceth@gmail.com>
2022-03-05 22:59:41 +0800

    add tests for airdrop && bug fixs

---
+++ contracts/test/BendTokenTester.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+import {BendToken} from "../token/BendToken.sol";
+
+contract BendTokenTester is BendToken {
+    function setBalance(address to, uint256 amount) public {
+        uint256 old = balanceOf(to);
+        if (old < amount) {
+            _mint(to, amount - old);
+        } else if (old > amount) {
+            _burn(to, old - amount);
+        }
+    }
+}
