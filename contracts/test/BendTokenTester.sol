2a8b8bffed836aef972835c427c90586d1c0c7b3
akashiceth <akashiceth@gmail.com>
2022-03-17 12:54:05 +0800

    upgrade compile version to 0.8.4

--- contracts/test/BendTokenTester.sol
+++ contracts/test/BendTokenTester.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity 0.8.0;
+pragma solidity 0.8.4;
 import {BendToken} from "../token/BendToken.sol";
 
 contract BendTokenTester is BendToken {
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ import { accounts } from "./test-wallets";
  */
 export default {
   solidity: {
-    version: "0.8.0",
+    version: "0.8.4",
     settings: {
       optimizer: { enabled: true, runs: 200 },
     },
