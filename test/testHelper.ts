21e711780849abf818fd5c990eb0b9110599d214
akashiceth <akashiceth@gmail.com>
2022-04-25 11:35:05 +0800

    add BEND/ETH uni staking incentive

--- test/testHelper.ts
+++ test/testHelper.ts
@@ -6,153 +6,15 @@ import {
   BigNumber,
 } from "ethers";
 import { expect } from "chai";
-import { waitForTx, makeBN, timeAtBlock } from "./utils";
+import {
+  waitForTx,
+  makeBN,
+  timeAtBlock,
+  mineBlockAndIncreaseTime,
+} from "./utils";
 import { signTypedData, SignTypedDataVersion } from "@metamask/eth-sig-util";
 import { fromRpcSig, ECDSASignature } from "ethereumjs-util";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { STAKED_TOKEN_NAME } from "./constants";
-
-export const buildGovPermitParams = (
-  chainId: number,
-  governance: string,
-  id: string,
-  support: boolean
-) => ({
-  types: {
-    EIP712Domain: [
-      { name: "name", type: "string" },
-      { name: "chainId", type: "uint256" },
-      { name: "verifyingContract", type: "address" },
-    ],
-    VoteEmitted: [
-      { name: "id", type: "uint256" },
-      { name: "support", type: "bool" },
-    ],
-  },
-  primaryType: "VoteEmitted" as const,
-  domain: {
-    name: "Governance",
-    version: "1",
-    chainId: chainId,
-    verifyingContract: governance,
-  },
-  message: {
-    id,
-    support,
-  },
-});
-
-export const buildPermitParams = (
-  chainId: number,
-  tokenContract: string,
-  tokenName: string,
-  owner: string,
-  spender: string,
-  nonce: number,
-  deadline: string,
-  value: BigNumber | number
-) => ({
-  types: {
-    EIP712Domain: [
-      { name: "name", type: "string" },
-      { name: "version", type: "string" },
-      { name: "chainId", type: "uint256" },
-      { name: "verifyingContract", type: "address" },
-    ],
-    Permit: [
-      { name: "owner", type: "address" },
-      { name: "spender", type: "address" },
-      { name: "value", type: "uint256" },
-      { name: "nonce", type: "uint256" },
-      { name: "deadline", type: "uint256" },
-    ],
-  },
-  primaryType: "Permit" as const,
-  domain: {
-    name: tokenName,
-    version: "1",
-    chainId: chainId,
-    verifyingContract: tokenContract,
-  },
-  message: {
-    owner,
-    spender,
-    value: value.toString(),
-    nonce,
-    deadline,
-  },
-});
-
-export const buildDelegateParams = (
-  chainId: number,
-  aaveToken: string,
-  delegatee: string,
-  nonce: string,
-  expiry: string
-) => ({
-  types: {
-    EIP712Domain: [
-      { name: "name", type: "string" },
-      { name: "version", type: "string" },
-      { name: "chainId", type: "uint256" },
-      { name: "verifyingContract", type: "address" },
-    ],
-    Delegate: [
-      { name: "delegatee", type: "address" },
-      { name: "nonce", type: "uint256" },
-      { name: "expiry", type: "uint256" },
-    ],
-  },
-  primaryType: "Delegate" as const,
-  domain: {
-    name: STAKED_TOKEN_NAME,
-    version: "1",
-    chainId: chainId,
-    verifyingContract: aaveToken,
-  },
-  message: {
-    delegatee,
-    nonce,
-    expiry,
-  },
-});
-
-export const buildDelegateByTypeParams = (
-  chainId: number,
-  aaveToken: string,
-  delegatee: string,
-  type: string,
-  nonce: string,
-  expiry: string
-) => ({
-  types: {
-    EIP712Domain: [
-      { name: "name", type: "string" },
-      { name: "version", type: "string" },
-      { name: "chainId", type: "uint256" },
-      { name: "verifyingContract", type: "address" },
-    ],
-    DelegateByType: [
-      { name: "delegatee", type: "address" },
-      { name: "type", type: "uint256" },
-      { name: "nonce", type: "uint256" },
-      { name: "expiry", type: "uint256" },
-    ],
-  },
-  primaryType: "DelegateByType" as const,
-  domain: {
-    name: STAKED_TOKEN_NAME,
-    version: "1",
-    chainId: chainId,
-    verifyingContract: aaveToken,
-  },
-  message: {
-    delegatee,
-    type,
-    nonce,
-    expiry,
-  },
-});
 
 export const getSignatureFromTypedData = (
   privateKey: string,
@@ -174,26 +36,16 @@ type AssetConfig = {
 export async function compareRewardsAtAction(
   stakedToken: Contract,
   userAddress: string,
-  actions: () => Promise<ContractTransaction>[],
-  shouldReward?: boolean,
-  assetConfig?: AssetConfig
+  action: () => Promise<void>,
+  shouldReward: boolean,
+  emissionPerSecond?: number
 ) {
+  if (emissionPerSecond) {
+    await stakedToken.configure(emissionPerSecond);
+  }
+  await stakedToken.updateCurrentUnclaimedRewards([userAddress]);
   const underlyingAsset = stakedToken.address;
-  const rewardsBalanceBefore = await stakedToken.getTotalRewardsBalance(
-    userAddress
-  );
-  // Configure assets of stake token
-  const assetConfiguration = assetConfig
-    ? {
-        ...assetConfig,
-        underlyingAsset,
-      }
-    : {
-        emissionPerSecond: "100",
-        totalStaked: await stakedToken.totalSupply(),
-        underlyingAsset,
-      };
-  await stakedToken.configureAssets([assetConfiguration]);
+  const rewardsBalanceBefore = await stakedToken.claimableRewards(userAddress);
   const userBalance = await stakedToken.balanceOf(userAddress);
   // Get index before actions
   const userIndexBefore = await getUserIndex(
@@ -201,10 +53,8 @@ export async function compareRewardsAtAction(
     userAddress,
     underlyingAsset
   );
-  // Dispatch actions that can or not update the user index
-  const receipts: ethers.ContractReceipt[] = await Promise.all(
-    actions().map(async (action) => await waitForTx(await action))
-  );
+
+  await action();
 
   // Get index after actions
   const userIndexAfter = await getUserIndex(
@@ -213,9 +63,7 @@ export async function compareRewardsAtAction(
     underlyingAsset
   );
   // Compare calculated JS rewards versus Solidity user rewards
-  const rewardsBalanceAfter = await stakedToken.getTotalRewardsBalance(
-    userAddress
-  );
+  const rewardsBalanceAfter = await stakedToken.claimableRewards(userAddress);
 
   const expectedAccruedRewards = getRewards(
     userBalance,
@@ -269,34 +117,25 @@ export async function compareRewardsAtTransfer(
   from: SignerWithAddress,
   to: SignerWithAddress,
   amount: BigNumberish,
-  fromShouldReward?: boolean,
-  toShouldReward?: boolean,
-  assetConfig?: AssetConfig
+  fromShouldReward: boolean,
+  toShouldReward: boolean,
+  emissionPerSecond?: number
 ) {
   const fromAddress = from.address;
   const toAddress = to.address;
   const underlyingAsset = stakedToken.address;
+  if (emissionPerSecond) {
+    await stakedToken.configure(emissionPerSecond);
+  }
 
   const fromSavedBalance = await stakedToken.balanceOf(fromAddress);
   const toSavedBalance = await stakedToken.balanceOf(toAddress);
-  const fromSavedRewards = await stakedToken.getTotalRewardsBalance(
+
+  await stakedToken.updateCurrentUnclaimedRewards([from.address, to.address]);
+  const fromRewardsBalanceBefore = await stakedToken.claimableRewards(
     fromAddress
   );
-  const toSavedRewards = await stakedToken.getTotalRewardsBalance(toAddress);
-
-  // Configure assets of stake token
-  const assetConfiguration = assetConfig
-    ? {
-        ...assetConfig,
-        underlyingAsset,
-      }
-    : {
-        emissionPerSecond: "100",
-        totalStaked: await stakedToken.totalSupply(),
-        underlyingAsset,
-      };
-  await stakedToken.configureAssets([assetConfiguration]);
-
+  const toRewardsBalanceBefore = await stakedToken.claimableRewards(toAddress);
   // Get index before actions
   const fromIndexBefore = await getUserIndex(
     stakedToken,
@@ -311,7 +150,6 @@ export async function compareRewardsAtTransfer(
 
   // Load actions that can or not update the user index
   await waitForTx(await stakedToken.connect(from).transfer(toAddress, amount));
-
   // Check rewards after transfer
 
   // Get index after actions
@@ -327,7 +165,7 @@ export async function compareRewardsAtTransfer(
   );
 
   // FROM: Compare calculated JS rewards versus Solidity user rewards
-  const fromRewardsBalanceAfter = await stakedToken.getTotalRewardsBalance(
+  const fromRewardsBalanceAfter = await stakedToken.claimableRewards(
     fromAddress
   );
   const fromExpectedAccruedRewards = getRewards(
@@ -335,21 +173,20 @@ export async function compareRewardsAtTransfer(
     fromIndexAfter,
     fromIndexBefore
   );
+
   expect(fromRewardsBalanceAfter).to.eq(
-    fromSavedRewards.add(fromExpectedAccruedRewards)
+    fromRewardsBalanceBefore.add(fromExpectedAccruedRewards)
   );
 
   // TO: Compare calculated JS rewards versus Solidity user rewards
-  const toRewardsBalanceAfter = await stakedToken.getTotalRewardsBalance(
-    toAddress
-  );
+  const toRewardsBalanceAfter = await stakedToken.claimableRewards(toAddress);
   const toExpectedAccruedRewards = getRewards(
     toSavedBalance,
     toIndexAfter,
     toIndexBefore
   );
   expect(toRewardsBalanceAfter).to.eq(
-    toSavedRewards.add(toExpectedAccruedRewards)
+    toRewardsBalanceBefore.add(toExpectedAccruedRewards)
   );
 
   // Explicit check rewards when the test case expects rewards to the user
