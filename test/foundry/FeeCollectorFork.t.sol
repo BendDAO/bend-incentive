c509be8045a89201c131de460fe4fb27edbd7311
akashiceth <akashiceth@gmail.com>
2023-08-20 10:51:42 +0800

    test: Add busdt upgrade fork test

--- test/foundry/FeeCollectorFork.t.sol
+++ test/foundry/FeeCollectorFork.t.sol
@@ -26,12 +26,17 @@ contract FeeCollectorForkTest is Test {
         0x4e4C314E2391A58775be6a15d7A05419ba7D2B6e;
     address constant proxyAdminAddress =
         0x859f6e05410893fe64BC84d92BdA773fF798cf66;
-    address constant feeCollectorAddress =
-        0xf3aB1d58Ce6B9E0D42b8958c918649305e1b1d26;
+
     address constant feeDistributorAddress =
         0x2338D34337dd0811b684640de74717B73F7B8059;
-    address constant BWETH = 0xeD1840223484483C0cb050E6fC344d1eBF0778a9;
-    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    FeeCollector feeCollector =
+        FeeCollector(0xf3aB1d58Ce6B9E0D42b8958c918649305e1b1d26);
+    IBToken bweth = IBToken(0xeD1840223484483C0cb050E6fC344d1eBF0778a9);
+    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IBToken busdt = IBToken(0x9631C79BfD6123A5B53307B6cdfb35F97606F954);
+    IERC20 usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    IBendCollector bendCollector = IBendCollector(feeCollector.bendCollector());
 
     // how to run this testcase
     // forge test -vvv --match-contract FeeCollectorFork0715Test --fork-url https://RPC --fork-block-number xxxxx
@@ -40,38 +45,33 @@ contract FeeCollectorForkTest is Test {
 
     function testFork_UpgradeFeeCollector() public {
         // upgrading fee collector
-        ProxyAdmin proxyAdmin = ProxyAdmin(proxyAdminAddress);
-
-        TransparentUpgradeableProxy feeCollectorProxy = TransparentUpgradeableProxy(
-                payable(feeCollectorAddress)
-            );
-        FeeCollector feeCollectorImpl = new FeeCollector();
         vm.startPrank(timelockControllerAddress);
-        proxyAdmin.upgrade(feeCollectorProxy, address(feeCollectorImpl));
+        ProxyAdmin(proxyAdminAddress).upgrade(
+            TransparentUpgradeableProxy(payable(address(feeCollector))),
+            address(new FeeCollector())
+        );
         vm.stopPrank();
 
-        FeeCollector feeCollector = FeeCollector(payable(feeCollectorAddress));
-        IBToken bweth = IBToken(BWETH);
-        IERC20 weth = IERC20(WETH);
-        IBendCollector bendCollector = IBendCollector(
-            feeCollector.bendCollector()
-        );
         vm.startPrank(lendingOwnerAddress);
-        bendCollector.approve(bweth, feeCollectorAddress, type(uint256).max);
+        bendCollector.approve(bweth, address(feeCollector), type(uint256).max);
+        bendCollector.approve(busdt, address(feeCollector), type(uint256).max);
         vm.stopPrank();
 
         vm.startPrank(incentiveOwnerAddress);
-        feeCollector.addBToken(BWETH);
-        feeCollector.setTreasuryPercentage(WETH, 5000);
+        feeCollector.addBToken(address(bweth));
+        feeCollector.setTreasuryPercentage(address(weth), 5000);
+        feeCollector.addBToken(address(busdt));
+        feeCollector.setTreasuryPercentage(address(usdt), 10000);
         vm.stopPrank();
 
         uint256 bwethAmount = bweth.balanceOf(address(bendCollector));
         console.log("bwethAmount:", bwethAmount);
-        uint256 wethAmount = weth.balanceOf(feeCollectorAddress);
+        uint256 wethAmount = weth.balanceOf(address(feeCollector));
         console.log("wethAmount:", wethAmount);
 
         wethAmount += bwethAmount;
 
+        // weth
         uint256 bwethBalanceOfTreasuryBeforeCollect = bweth.balanceOf(
             feeCollector.treasury()
         );
@@ -86,6 +86,28 @@ contract FeeCollectorForkTest is Test {
             feeCollector.treasury()
         );
 
+        // usdt
+        uint256 busdtAmount = busdt.balanceOf(address(bendCollector));
+        console.log("busdtAmount:", busdtAmount);
+        uint256 usdtAmount = usdt.balanceOf(address(feeCollector));
+        console.log("usdtAmount:", usdtAmount);
+
+        usdtAmount += busdtAmount;
+
+        uint256 busdtBalanceOfTreasuryBeforeCollect = busdt.balanceOf(
+            feeCollector.treasury()
+        );
+        uint256 busdtBalanceOfDistributorBeforeCollect = busdt.balanceOf(
+            feeDistributorAddress
+        );
+
+        uint256 usdtBalanceOfDistributorBeforeCollect = usdt.balanceOf(
+            feeDistributorAddress
+        );
+        uint256 usdtBalanceOfTreasuryBeforeCollect = usdt.balanceOf(
+            feeCollector.treasury()
+        );
+
         // collect bweth to distributor
         feeCollector.collect();
 
@@ -94,7 +116,7 @@ contract FeeCollectorForkTest is Test {
         // check bweth results
         {
             assertEq(
-                bweth.balanceOf(feeCollectorAddress),
+                bweth.balanceOf(address(feeCollector)),
                 0,
                 "fee collector bweth delta not zero"
             );
@@ -112,18 +134,38 @@ contract FeeCollectorForkTest is Test {
                 0,
                 "fee distributor bweth delta not zero"
             );
+
+            assertEq(
+                busdt.balanceOf(address(feeCollector)),
+                0,
+                "fee collector busdt delta not zero"
+            );
+
+            assertEq(
+                busdt.balanceOf(feeCollector.treasury()) -
+                    busdtBalanceOfTreasuryBeforeCollect,
+                0,
+                "treasury busdt delta not zero"
+            );
+
+            assertEq(
+                busdt.balanceOf(feeDistributorAddress) -
+                    busdtBalanceOfDistributorBeforeCollect,
+                0,
+                "fee distributor busdt delta not zero"
+            );
         }
 
         // check weth results
         {
             uint256 wethAmountToTreasury = wethAmount.percentMul(
-                feeCollector.getTreasuryPercentage(WETH)
+                feeCollector.getTreasuryPercentage(address(weth))
             );
             uint256 wethAmountToDistributor = (wethAmount -
                 wethAmountToTreasury);
 
             assertEq(
-                weth.balanceOf(feeCollectorAddress),
+                weth.balanceOf(address(feeCollector)),
                 0,
                 "treasury weth not match"
             );
@@ -140,6 +182,32 @@ contract FeeCollectorForkTest is Test {
                     wethBalanceOfDistributorBeforeCollect,
                 wethAmountToDistributor,
                 "distributor weth not match"
+            );
+
+            uint256 usdtAmountToTreasury = usdtAmount.percentMul(
+                feeCollector.getTreasuryPercentage(address(usdt))
+            );
+            uint256 usdtAmountToDistributor = (usdtAmount -
+                usdtAmountToTreasury);
+
+            assertEq(
+                usdt.balanceOf(address(feeCollector)),
+                0,
+                "treasury usdt not match"
+            );
+
+            assertEq(
+                usdt.balanceOf(feeCollector.treasury()) -
+                    usdtBalanceOfTreasuryBeforeCollect,
+                usdtAmountToTreasury,
+                "treasury usdt not match"
+            );
+
+            assertEq(
+                usdt.balanceOf(feeDistributorAddress) -
+                    usdtBalanceOfDistributorBeforeCollect,
+                usdtAmountToDistributor,
+                "distributor usdt not match"
             );
         }
     }
